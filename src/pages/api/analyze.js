0b664d2cbabc2d4cbfcbd20f322c66da01ed5413
Jeremy Vejgman <jdv2653@gmail.com>
2025-02-04 16:46:20 -0500

    Specified analyze.js runtime and route for vercel

--- src/pages/api/analyze.js
+++ src/pages/api/analyze.js
@@ -1,5 +1,9 @@
 export const prerender = false;
 
+export const config = {
+  runtime: 'nodejs',
+};
+
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const Wappalyzer = require('wappalyzer-rm');
