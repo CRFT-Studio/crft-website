53c20bdb0d9532255df863743c989523fa36fac3
Jeremy Vejgman <jdv2653@gmail.com>
2025-03-22 04:30:22 -0400

    Updated site url in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://crft.studio',
+  site: 'https://www.crft.studio',
   integrations: [tailwind(), sitemap(), mdx(), robotsTxt()],
   output: "static",
   adapter: vercel(),
