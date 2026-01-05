61a60b9da31ef01b5d5e40c4c9ebc6c5911c7dba
Jeremy Vejgman <jdv2653@gmail.com>
2026-01-05 01:46:58 -0500

    Installed sanity and react

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,12 +12,15 @@ import vercel from "@astrojs/vercel";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.crft.studio',
   integrations: [sitemap({
       filter: (page) => !new URL(page).pathname.startsWith('/proposal'),
-  }), mdx(), robotsTxt()],
+  }), mdx(), robotsTxt(), sanity(), react()],
   output: "static",
   adapter: vercel(),
   trailingSlash: "never",
